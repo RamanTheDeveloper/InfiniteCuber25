59f8b09f9c0c1419db7cd7107aabf29389ad82fa
RamanTheDeveloper <ramandeep-singh25@outlook.com>
2022-04-13 14:54:52 +0200

    Text added to background

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -7,8 +7,8 @@ function Homepage() {
   return (
     <>
         <div className='w-full h-max bg-gradient-to-r from-gray-500 to-gray-500 z-30'>
-            <img src={bg_img} alt="background" className='bg-center bg-no-repeat bg-cover flex items-center justify-center h-full relative'/>
-            <div className='z-30 absolute flex justify-center align-middle text-white font-medium text-lg animate-fade'>
+            <img src={bg_img} alt="background" className='bg-center bg-no-repeat bg-cover flex items-center justify-center absolute z-10 w-full'/>
+            <div className='z-30 w-full h-full absolute flex justify-center align-middle text-white font-medium text-3xl animate-fade'>
                 <h1>Welcome Cuber</h1>
             </div>
         </div>

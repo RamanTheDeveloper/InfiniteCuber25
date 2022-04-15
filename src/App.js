02d85cb1fe033b671b92176a6d9bafc9f7ba87eb
RamanTheDeveloper <ramandeep-singh25@outlook.com>
2022-04-15 19:23:46 +0200

    Cards section on Homepage created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Homepage from './components/homepage/Homepage';
+import InfoCards from './components/homepage/InfoCards';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Navbar/>
       <Homepage/>
+      <InfoCards/>
     </>
   );
 }
---
+++ src/components/homepage/InfoCards.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function InfoCards() {
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default InfoCards

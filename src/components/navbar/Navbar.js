7b7ae44ec77b47772a1c90045a63656a37564002
RamanTheDeveloper <ramandeep-singh25@outlook.com>
2022-04-15 19:08:11 +0200

    Bug fix

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
 
 
   return (
-    <div className='sticky flex flex-row justify-center align-middle flex-wrap bg-black w-full h-24 z-20 py-2 px-2'>
+    <div className='sticky flex flex-row justify-center align-middle flex-wrap bg-black w-full h-24 z-20 py-6 px-2'>
       <a href='/#'><img src={logo} alt="Logo" className='w-[145px] h-[76px] cursor-pointer absolute left-20'/></a>
       <ul className='flex justify-center align-middle list-none text-center pt-6'>
         <li><a href='/#' className='m-8 text-white decoration-none text-base font-semibold uppercase hover:text-black hover:bg-white hover:rounded-2xl hover:px-6 hover:py-2 hover:transition-all ease-in-out duration-300'>Tutorials</a></li>

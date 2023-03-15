b1faec0851185cba7bfde956428ea018e863ed06
Sing6709 <ramandeep-singh25@outlook.com>
2023-03-15 19:09:59 +0100

    Styled info cards borders

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -7,7 +7,7 @@ function Homepage() {
   return (
     <>
         <div className='w-full h-screen flex justify-center align-middle z-30 bg-gradient-to-b from-black to-black-100'>
-            <h1 className='text-white z-30 font-bold text-5xl text-center w-full animate-fade-text lg:translate-y-[30rem] md:translate-y-[15rem]'>Welcome Cuber</h1>
+            <h1 className='text-white z-30 font-bold text-5xl text-center w-auto h-auto animate-fade-text lg:translate-y-[30rem] md:translate-y-[15rem]'>Welcome Cuber</h1>
             <img src={bg_img} alt="background" className='bg-center bg-no-repeat bg-cover absolute z-10 mix-blend-overlay'/>
         </div>
         
--- src/components/homepage/InfoCards.js
+++ src/components/homepage/InfoCards.js
@@ -9,7 +9,7 @@ function InfoCards() {
         <div className='w-full my-[10rem]'>
           <div className='flex flex-row gap-16 justify-center align-middle px-5'>
             <IconContext.Provider value={{size:"30px"}}>
-            <div className='w-[20rem] h-max flex flex-col gap-3'>
+            <div className='w-[20rem] h-max flex flex-col gap-3 border-2 border-solid rounded-lg shadow-md p-4'>
               <div className='flex flex-row gap-3'>
                 <FaIcon.FaCubes/>
                 <p className='text-2xl font-bold pb-2'>Cubing Resources</p>
@@ -17,17 +17,17 @@ function InfoCards() {
               <p>This website has cubing resources where you can find all the necessary information about cubing whether you're a beginner or an advanced cuber. I will link other websites as well as relevant YouTube video's so you don't have to go looking for them.</p>
             </div>
 
-            <div className='w-[20rem] h-max flex flex-col gap-3'>
+            <div className='w-[20rem] h-max flex flex-col gap-3 border-2 border-solid rounded-lg shadow-md p-4'>
               <div className='flex flex-row gap-3'>
                 <AiIcon.AiFillFileText/>
                 <p className='text-2xl font-bold pb-2'>Handy Algorithms</p>
               </div>
               <p>
-              Under the Algorithms tab, you can find all the algs for all WCA puzzles including a downloadable sheet. The algs will have explainations wherever necessary.
+                Under the Algorithms tab, you can find all the algs for all WCA puzzles including a downloadable sheet. The algs will have explainations wherever necessary.
               </p>
             </div>
 
-            <div className='w-[20rem] h-max flex flex-col gap-3'>
+            <div className='w-[20rem] h-max flex flex-col gap-3 border-2 border-solid rounded-lg shadow-md p-4'>
               <div className='flex flex-row gap-3'>
                 <AiIcon.AiFillFolder/>
                 <p className='text-2xl font-bold pb-2'>Documenting My Journey</p>

27755a1cf99d6b8ad6eca88b9f926727afb57c2a
Ramandeep Singh <ramandeep-singh25@outlook.com>
2024-02-06 16:33:40 +0100

    Bg image changed on homepage

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -2,12 +2,11 @@ import React from 'react'
 
 function Homepage() {
 
-    const bg_img = require('../images/homepage/rubiks-cube-background.png')
+    const bg_img = require('../images/homepage/infinitecuber25-logo-bg.jpg')
 
   return (
     <>
-        <div className='w-full h-screen flex justify-center align-middle z-30 bg-gradient-to-b from-black to-black-100'>
-            <h1 className='text-white z-30 font-bold text-5xl text-center w-auto h-auto animate-fade-text lg:translate-y-[30rem] md:translate-y-[15rem]'>Welcome Cuber</h1>
+        <div className='w-full h-screen flex justify-center align-middle z-30'>
             <img src={bg_img} alt="background" className='bg-center bg-no-repeat bg-cover absolute z-10 mix-blend-overlay'/>
         </div>
         
